6f4d88ead2a3e893f6b80e526c2cca0c547c44ea
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-01-02 20:37:13 -0300

    refactor: rewrite new_cars_view function for CBV

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path
 
 from accounts.views import login_view, logout_view, register_view
-from cars.views import new_car_view, CarsView
+from cars.views import CarsView, NewCarView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -29,5 +29,5 @@ urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
     path("cars/", CarsView.as_view(), name="cars_list"),
-    path("new_car/", new_car_view, name="new_car"),
+    path("new_car/", NewCarView.as_view(), name="new_car"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
