83fb2937ce9a3f1d7225437f1633ed7042583cb5
Elaine Ferreira <elaineferreira.dev@gmail.com>
2025-01-02 20:22:12 -0300

    refactor: rewrite cars_view fuction for CBV

--- app/urls.py
+++ app/urls.py
@@ -21,13 +21,13 @@ from django.contrib import admin
 from django.urls import path
 
 from accounts.views import login_view, logout_view, register_view
-from cars.views import cars_view, new_car_view
+from cars.views import new_car_view, CarsView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
-    path("cars/", cars_view, name="cars_list"),
+    path("cars/", CarsView.as_view(), name="cars_list"),
     path("new_car/", new_car_view, name="new_car"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import redirect, render
+from django.views import View
 
 from .forms import CarModelForm
 from .models import Car
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by("model")
-    search = request.GET.get("search")
+class CarsView(View):
+    def get(self, request):
+        cars = Car.objects.all().order_by("model")
+        search = request.GET.get("search")
 
-    if search:
-        cars = cars.filter(model__icontains=search)
+        if search:
+            cars = cars.filter(model__icontains=search)
 
-    return render(request, "cars.html", {"cars": cars})
+        return render(request, "cars.html", {"cars": cars})
 
 
 def new_car_view(request):
